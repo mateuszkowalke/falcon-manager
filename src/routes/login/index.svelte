844d9fc17a26517df69c7e7da7e8e1878ee0ef15
Mateusz Kowalke <mateusz.kowalke@aurafutures.com>
2021-12-16 13:50:30 +0100

    fix: removed unneeded demo code

--- src/routes/login/index.svelte
+++ src/routes/login/index.svelte
@@ -1,25 +1,4 @@
 <script context="module" lang="ts">
-	import { enhance } from '$lib/form';
-	import type { Load } from '@sveltejs/kit';
-
-	// see https://kit.svelte.dev/docs#loading
-	export const load: Load = async ({ fetch }) => {
-		const res = await fetch('/login.json');
-
-		if (res.ok) {
-			const todos = await res.json();
-
-			return {
-				props: { todos }
-			};
-		}
-
-		const { message } = await res.json();
-
-		return {
-			error: new Error(message)
-		};
-	};
 </script>
 
 <script lang="ts">
