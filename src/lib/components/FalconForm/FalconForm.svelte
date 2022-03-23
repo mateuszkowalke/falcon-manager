1554a6bab9beb8aa431e82e1049687ecd56d80b1
Mateusz Kowalke <mateusz.kowalke@aurafutures.com>
2022-03-23 06:21:24 +0100

    feature: adding required fields to falon form

--- src/lib/components/FalconForm/FalconForm.svelte
+++ src/lib/components/FalconForm/FalconForm.svelte
@@ -1,3 +1,42 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+	export const load: Load = async function ({ params, fetch }) {
+		const speciesUrl = `/api/species`;
+		const speciesRes = await fetch(speciesUrl);
+
+		if (!speciesRes.ok) {
+			return {
+				status: speciesRes.status,
+				error: new Error(`Could not load ${speciesUrl}`)
+			};
+		}
+
+		const breedingProjectsUrl = `/api/species`;
+		const breedingProjectsRes = await fetch(speciesUrl);
+
+		if (!breedingProjectsRes.ok) {
+			return {
+				status: breedingProjectsRes.status,
+				error: new Error(`Could not load ${breedingProjectsUrl}`)
+			};
+		}
+
+        // todo
+        // request aviaries
+		const species = await speciesRes.json();
+		const breedingProjects = await breedingProjectsRes.json();
+		// todo
+		// create endpoints for listing species, breedingProjects and aviaries per user
+
+		return {
+			props: {
+				species,
+				breedingProjects
+			}
+		};
+	};
+</script>
+
 <script lang="ts">
 	import DatePicker from '$lib/DatePicker/DatePicker.svelte';
 	import type { Aviary, Falcon, Species } from '@prisma/client';
@@ -18,6 +57,14 @@
 	<input type="text" name="ring" bind:value={falcon.ring} />
 	<label for="sex">Sex:</label>
 	<select name="sex" bind:value={sex}>
+		{#each ['male', 'female', 'unknown'] as opt}
+			<option value={opt}>
+				{opt}
+			</option>
+		{/each}
+	</select>
+	<label for="species">Species:</label>
+	<select name="species" bind:value={sex}>
 		{#each ['male', 'female', 'unknown'] as opt}
 			<option value={opt}>
 				{opt}
